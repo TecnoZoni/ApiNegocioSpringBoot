ccb5d3b0cb5d05e6cf40dd34339ab4d35b73c9dd
Admin <agustinminzoni@gmail.com>
2024-07-14 23:39:19 -0300

    Primer Commit

---
+++ src/main/java/com/negocio/negocioSpringBoot/NegocioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.negocio.negocioSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/negocio/negocioSpringBoot/NegocioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negocio.negocioSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegocioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
